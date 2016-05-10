df02ce01b71ec2230c7ed68d3b46590564bee0db
Pasi Junttila <pasi.junttila@live.com>
2016-05-10 08:04:47 +0300

    edit step 1
    css\editfood.css   NEW  basically a copy of addfood.css
    js\editfood.js      NEW  editform event handling, onRendered and helpers
    js\fab.js           double click event, helper for edit rendering
    js\mockup.js        fuuditest.update method
    pages\foods\editfood.html   NEW   food item edit form
    routes\routes.js         route for edit

--- js/fab.js
+++ js/fab.js
@@ -3,7 +3,14 @@ if (Meteor.isClient) {
 	Template.foods.events({
 		'click .fab': function(event) {
 			Router.go('/foods/add')
-		},
+	    },
+	    'dblclick .food-item': function (event) {
+	        console.log("T foods: dblclick");
+	        //console.log(this._id); console.log(this.valmistaja);
+	        console.log(this.name);
+	        Session.set('docID', this);
+	        Router.go('/foods/edit')
+	    },
 	});
 
 	Template.food.events({
@@ -23,4 +30,13 @@ if (Meteor.isClient) {
 			Router.go('/foods/add')
 		},
 	});
+	Template.editform.helpers({
+	    formData: function () {
+	        console.log("editfood helper");
+	        var document = Session.get('docID');
+	        console.log(document._id);
+	        return document;
+	        //return fuuditest.findOne({ _id: document });
+	    }
+	})
 }
