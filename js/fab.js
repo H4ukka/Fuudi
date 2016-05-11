703f637889bb8d56f32c686b17053cd56c1f6d64
Pasi Junttila <pasi.junttila@live.com>
2016-05-11 09:41:26 +0300

    editform helper modified
    editform helper now stores (Session) document and image filename
    attached to it

--- js/fab.js
+++ js/fab.js
@@ -5,10 +5,8 @@ if (Meteor.isClient) {
 			Router.go('/foods/add')
 	    },
 	    'dblclick .food-item': function (event) {
-	        console.log("T foods: dblclick");
-	        //console.log(this._id); console.log(this.valmistaja);
-	        console.log(this.name);
-	        Session.set('docID', this);
+	        //console.log("T foods: dblclick"); console.log(this.name);
+	        Session.set('theDoc', this);
 	        Router.go('/foods/edit')
 	    },
 	});
@@ -32,9 +30,17 @@ if (Meteor.isClient) {
 	});
 	Template.editform.helpers({
 	    formData: function () {
-	        console.log("editfood helper");
-	        var document = Session.get('docID');
-	        console.log(document._id);
+	        var document = Session.get('theDoc');
+            if (document.image_id !== undefined && document.image_id !== 0) { // undef for fineli items and 0 for items added with app
+                var queryRes = Images.find({ _id: document.image_id }).fetch(); //, {"original.name":1}); // it's an embedded document
+                var docFileNameArr = queryRes.map(function (doc) { return doc.original.name; });
+                if (docFileNameArr.length > 0) {  // found it
+                    Session.set('docFileName', docFileNameArr[0]);  //console.log(docFileName);
+                }
+            }
+            else {
+                Session.set('docFileName', '');
+            }
 	        return document;
 	        //return fuuditest.findOne({ _id: document });
 	    }
